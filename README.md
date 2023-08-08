e282d11ba81d107282c852f452b01f22e9e5b832
stephe-hu <stephen.hu@stonybrook.edu>
2023-08-08 22:50:00 +0000

    ive updated some files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hha_506_class9
 this is a test repo
+
+## Some python example code:
+```
+variable = 123
+```
---
+++ fake.py
@@ -0,0 +1,9 @@
+## this is a fake file 
+
+var1 = 1
+var2 = '2'
+var3 = [1, 2, 3]
+var4 = {
+    'firstName': 'hants',
+    'lastName': 'williams'
+}
