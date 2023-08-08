83a94df445ee24543d60fb5c5df7571ad44e8d87
stephe-hu <stephen.hu@stonybrook.edu>
2023-08-08 23:24:29 +0000

    ive added my code with module examples

---
+++ main.py
@@ -0,0 +1,10 @@
+import module1
+
+output1 = module1.addition(5, 10)
+output2 = module1.subtraction(5, 10)
+
+print('Output 1: ', output1)
+print('Output 2: ', output2)
+
+print(f'Output 1: {output1}')
+print(f'Output 2: {output2}')
---
+++ module1.py
@@ -0,0 +1,7 @@
+def addition(var1, var2):
+    output = var1 + var2
+    return output
+
+def subtraction(var1, var2):
+    output = var1 - var2
+    return output
