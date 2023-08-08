264521b5e40a0f5970655a428ecb41f043706f90
stephe-hu <stephen.hu@stonybrook.edu>
2023-08-08 23:31:45 +0000

    made new changes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import module1
+# import module1
+# import module1 as mod1
+# import module1 import *
+# import module1 import each individual function
+from module1 import addition as add, subtraction as sub
 
-output1 = module1.addition(5, 10)
-output2 = module1.subtraction(5, 10)
+output1 = add(5, 10)
+output2 = sub(5, 10)
 
 print('Output 1: ', output1)
 print('Output 2: ', output2)
