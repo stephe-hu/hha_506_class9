1cc93e81892f863a6ffa03d56a1358d4cad83acb
stephe-hu <stephen.hu@stonybrook.edu>
2023-08-09 00:53:18 +0000

    updated

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # import module1 as mod1
 # import module1 import *
 # import module1 import each individual function
-from module1 import addition as add, subtraction as sub
+# from module1 import addition as add, subtraction as sub
 
-output1 = add(5, 10)
-output2 = sub(5, 10)
+import sys
+sys.path.append('/home/stephen_hu/hha_506_class9/modules')
+import module1
+
+output1 = module1.addition(5, 10)
+output2 = module1.subtraction(5, 10)
 
 print('Output 1: ', output1)
 print('Output 2: ', output2)
---
+++ systemChecks.py
@@ -0,0 +1,19 @@
+import sys as mysystem
+import pandas as pd
+import sqlalchemy as sqlalchemy
+import numpy as np
+import tensorboard as tb
+print(mysystem.path)
+
+dataframe = pd.read_csv('')
+
+output = [
+    '/home/stephen_hu/hha_506_class9', 
+    '/usr/lib/python39.zip', 
+    '/usr/lib/python3.9', 
+    '/usr/lib/python3.9/lib-dynload', 
+    '/home/stephen_hu/.local/lib/python3.9/site-packages', 
+    '/usr/local/lib/python3.9/dist-packages', 
+    '/usr/lib/python3/dist-packages'
+
+]
